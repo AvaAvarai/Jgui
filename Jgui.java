73f9edc70272776d1afd7c4186b32f9659183a78
Alice Williams <alice.williams.tech@gmail.com>
2022-12-16 20:38:36 -0800

    Current working code

---
+++ Jgui.java
@@ -0,0 +1,45 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.lang.Runnable;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class Jgui implements Runnable {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Jgui());
+    }
+
+    @Override
+    public void run() {
+        JGuiFrame frame = new JGuiFrame();
+    }
+
+    public class JGuiFrame extends JFrame {
+        public JGuiFrame() {
+            super("TITLE HERE");
+            setPreferredSize(new Dimension(640, 480));
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            add(new JGuiPanel());
+            pack();
+            setLocationByPlatform(true);
+            setVisible(true);
+        }
+    }
+
+    public class JGuiPanel extends JPanel {
+        public JGuiPanel() {
+            super();
+            setPreferredSize(new Dimension(640, 480));
+            setBackground(Color.DARK_GRAY);
+            setVisible(true);
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+        }
+    }
+}
+
