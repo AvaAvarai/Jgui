21167ded2fb127e87fe4531ca86904d81bab7857
Alice Williams <alice.williams.tech@gmail.com>
2022-12-16 21:23:50 -0800

    add pseudo zooming

--- Jgui.java
+++ Jgui.java
@@ -2,6 +2,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.lang.Runnable;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
@@ -28,22 +30,38 @@ public class Jgui implements Runnable {
         }
     }
 
-    public class JGuiPanel extends JPanel {
+    public class JGuiPanel extends JPanel implements MouseWheelListener {
+        
+        int zoom;
+
         public JGuiPanel() {
             super();
+            zoom = 20;
             setPreferredSize(new Dimension(640, 480));
             setBackground(Color.DARK_GRAY);
+            addMouseWheelListener(this);
             setVisible(true);
         }
+
+        @Override
+	    public void mouseWheelMoved(MouseWheelEvent e) {
+		    int dir = e.getWheelRotation();
+            if (dir > 0 && zoom < 200) {
+               zoom+=5; 
+            } else if (zoom > 5) {
+               zoom-=5;
+            }
+            repaint();
+	    }
 
         @Override
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
             g.setColor(Color.LIGHT_GRAY);
-            for (int i = 0; i < getWidth(); i+=20) {
+            for (int i = 0; i < getWidth(); i+=zoom) {
                 g.drawLine(i, 0, i, getHeight());
             }
-            for (int i = 0; i < getHeight(); i+=20) {
+            for (int i = 0; i < getHeight(); i+=zoom) {
                 g.drawLine(0, i, getWidth(), i);
             }
         }
